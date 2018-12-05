2e7daf47e86939f638b05055039df42dc96b73ee
Ernest Fraiman <upapaus@gmail.com>
2018-12-05 11:36:28 +0000

    fixed weird behaviour

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const createRouter = function (collection) {
   const router = express.Router();
-  console.log(",2222", router);
+  // console.log("createRouter created", router);
 
-  router.get('/bb', (req, res) => {
-    console.log(",3333");
+  router.get('/', (req, res) => {
+    // console.log(",3333");
     res.json({'message': 'Hello Steven'});
   });
   return router;
--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ const createRouter = require('./helpers/create_router.js');
 
 const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
-app.use(bodyParser.json);
-const bucketRouter = createRouter("ooo");
-app.use('/api/bucket', bucketRouter);
+app.use(bodyParser.json());
+const bucketRouter = createRouter([]);
+ app.use('/api/bucket', bucketRouter);
 
 
 app.listen(3000, function () {
