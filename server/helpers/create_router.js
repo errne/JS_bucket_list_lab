4120fe69284d4ab34b94f71d3b463568adf9f19a
Ernest Fraiman <upapaus@gmail.com>
2018-12-05 13:22:50 +0000

    got GET / to list all items from mongo

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const ObjectID = require("mongodb").ObjectID;
 
 const createRouter = function (collection) {
   const router = express.Router();
   // console.log("createRouter created", router);
 
   router.get('/', (req, res) => {
-    // console.log(",3333");
-    res.json({'message': 'Hello Steven'});
+    collection
+    .find()
+    .toArray()
+    .then((docs) => res.json(docs))
+    .catch((err) => {
+      res.status(500);
+      res.json({status: 500, error: err});
+    });
   });
   return router;
 }
--- server/server.js
+++ server/server.js
@@ -2,16 +2,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
 
 
 const publicPath = path.join(__dirname, '../client/public');
+
 app.use(express.static(publicPath));
 app.use(bodyParser.json());
-const bucketRouter = createRouter([]);
- app.use('/api/bucket', bucketRouter);
 
+
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) => {
+  const db = client.db("bucketList");
+  const itemCollection = db.collection("items");
+  const bucketRouter = createRouter(itemCollection);
+  app.use('/api/bucketlist', bucketRouter);
+})
 
 app.listen(3000, function () {
-  console.log('It works');
+  console.log('Listening on port 3000');
 })
